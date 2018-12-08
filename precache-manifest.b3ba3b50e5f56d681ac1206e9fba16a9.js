47f253aeff20543475432ef52b53c9c9a6dc3a81
Abdulrahim Naser <abdulrahim.naser@hotmail.com>
2018-12-08 20:29:54 +0400

    Updates

---
+++ precache-manifest.b3ba3b50e5f56d681ac1206e9fba16a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GiftHub/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f7e45026a8b67a9cebb0",
+    "url": "/GiftHub/static/js/runtime~main.f7e45026.js"
+  },
+  {
+    "revision": "33a0289c7dd0ee626480",
+    "url": "/GiftHub/static/js/main.33a0289c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/GiftHub/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "33a0289c7dd0ee626480",
+    "url": "/GiftHub/static/css/main.fa3f5e38.chunk.css"
+  },
+  {
+    "revision": "edec7138e59b219f609bec127960f4c7",
+    "url": "/GiftHub/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ec1b234b398004f77d527d658dc1e3a.js"
+  "/GiftHub/precache-manifest.b3ba3b50e5f56d681ac1206e9fba16a9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/GiftHub/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
